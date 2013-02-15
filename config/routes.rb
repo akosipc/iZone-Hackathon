aaacb96d5ed71a51798ff4506854799b0bfab74a
Adrian Peterson Co <adrianpco@gmail.com>
2013-02-16 01:21:55 +0800

    Adds basic for features

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ IZoneHackathon::Application.routes.draw do
   resources :facebook_pull
 
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  mount Foundation::Icons::Rails::Engine => '/fi'
 
   resources :applicants
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }, :skip => [:sessions] do
