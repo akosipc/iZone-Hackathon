f5bb8f779d2af4a12c9d5f3bdce229613804a671
Adrian Peterson Co <adrianpco@gmail.com>
2013-02-16 02:27:22 +0800

    Adds pricing tab

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 IZoneHackathon::Application.routes.draw do
-  
+  get "applicants/index"
+  resources :facebook_pull
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
+  resources :applicants
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }, :skip => [:sessions] do
     delete 'users/sign_out' => 'devise/sessions#destroy', :as => 'destroy_user_session'
   end
 
   authenticated do
-    root to: 'pages#index'
+    root to: 'pages#search'
   end
 
   root to: 'pages#index'
