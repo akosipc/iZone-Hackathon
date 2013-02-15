043de9dade85e83159a5472e98c392d82aaf3677
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2013-02-15 22:39:49 +0800

    Added Facebook Pull Functions

--- app/controllers/facebook_pull_controller.rb
+++ app/controllers/facebook_pull_controller.rb
@@ -1,10 +1,29 @@
 class FacebookPullController < ApplicationController
 
   def index
-      @user = User.find params[:id]
+      @user = User.last
       @graph = Koala::Facebook::GraphAPI.new(@user.access_token)
 
-      @profile = @graph.get_object("me")
+      @graph.batch do |batch_api|
+        #personal details
+        @profile = batch_api.get_object("me")
+
+        #mutual friends (change this to reflect HR Groups username)
+        @friends = batch_api.get_connections("me", "mutualfriends/Robbie.Mars")
+
+        @likes = batch_api.get_connections("me","likes")
+        #user likes
+
+        #user posts
+        @photos = batch_api.get_connections("me","photos")
+
+        #user groups
+        @groups = batch_api.get_connections("me","groups")
+
+        #user events
+        @events = batch_api.get_connections("me","events")
+      end
+
   end
 
 end
